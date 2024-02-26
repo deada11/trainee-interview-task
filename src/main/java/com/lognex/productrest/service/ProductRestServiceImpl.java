bc2bc10da5b08fff321721b4300fa99b66f25c47
pnikitin <pnikitin@moysklad.ru>
2024-02-26 21:29:34 +0300

    Test

---
+++ src/main/java/com/lognex/productrest/service/ProductRestServiceImpl.java
@@ -0,0 +1,137 @@
+package com.lognex.productrest.service;
+
+import com.lognex.productrest.dao.ProductRepository;
+import com.lognex.productrest.entity.Product;
+import com.lognex.productrest.exception.CustomEntityNotFoundException;
+import com.lognex.productrest.validator.ProductValidator;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.Query;
+import jakarta.persistence.TypedQuery;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class ProductRestServiceImpl implements ProductRestService{
+
+    public ProductRestServiceImpl(){}
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Autowired
+    private EntityManager entityManager;
+
+    public ProductRestServiceImpl(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+    CriteriaQuery<Product> criteriaQuery = criteriaBuilder.createQuery(Product.class);
+    Root<Product> root = criteriaQuery.from(Product.class);
+
+    public void createProduct(Product product) {
+        ProductValidator productValidator = new ProductValidator();
+        productValidator.priceChanger(product);
+        productRepository.save(product);
+    }
+
+    public List<Product> readAllProducts(String name,
+                                         BigDecimal lessThanPrice,
+                                         BigDecimal greaterThanPrice,
+                                         Boolean availability,
+                                         String sortBy,
+                                         Integer limit) {
+
+        // В отдельный метод фильтрацию и сортировку.
+        // На вход передавать имя, цену, наличие, сортбай.
+        // Возвращает CriteriaQuery
+        // в виде criteriaQuery.where(criteria)
+
+        Predicate criteria = criteriaBuilder.conjunction();
+        if (name != null) {
+            criteria = criteriaBuilder.and(criteria,
+                    criteriaBuilder.like(criteriaBuilder.lower(root.get("name")), "%" + name + "%".toLowerCase()));
+        }
+
+        if (greaterThanPrice != null) {
+            criteria = criteriaBuilder.and(criteria,
+                    criteriaBuilder.greaterThanOrEqualTo(root.get("price"), greaterThanPrice));
+        }
+
+        if (lessThanPrice != null) {
+            criteria = criteriaBuilder.and(criteria,
+                    criteriaBuilder.lessThanOrEqualTo(root.get("price"), lessThanPrice));
+        }
+
+        if (availability != null) {
+            criteria = criteriaBuilder.and(criteria,
+                    criteriaBuilder.equal(root.get("availability"), availability));
+        }
+
+        criteriaQuery.where(criteria);
+
+        if (sortBy != null) {
+            if (sortBy.equalsIgnoreCase("name")) {
+                criteriaQuery.orderBy(criteriaBuilder.asc(root.get("name")));
+            }
+            if (sortBy.equalsIgnoreCase("price")) {
+                criteriaQuery.orderBy(criteriaBuilder.asc(root.get("price")));
+            }
+        } else {
+            criteriaQuery.orderBy(criteriaBuilder.asc(root.get("price")));
+        }
+
+        TypedQuery<Product> query = entityManager.createQuery(criteriaQuery);
+        if (limit != null) {
+            query.setMaxResults(limit);
+        }
+        return query.getResultList();
+    }
+
+
+    public Product readProduct(UUID id) {
+        Optional<Product> product = productRepository.findById(id);
+
+        if (!product.isPresent()) {
+            throw new CustomEntityNotFoundException(id);
+        }
+        return product.get();
+    }
+
+    public void updateProduct(Product product, UUID id) {
+        Optional<Product> p = productRepository.findById(id);
+        if (!p.isPresent())
+            throw new CustomEntityNotFoundException(id);
+        Product updatebleProduct = p.get();
+        productRepository.save(updater(product, updatebleProduct));
+    }
+
+    public void deleteProduct(UUID id) {
+        Product deletingProduct = productRepository.findById(id).orElseThrow(() -> new CustomEntityNotFoundException(id));
+        productRepository.deleteById(id);
+    }
+
+    private Product updater(Product product, Product updatableProduct) {
+        ProductValidator productValidator = new ProductValidator();
+
+        updatableProduct.setName(product.getName());
+        updatableProduct.setDescription(product.getDescription());
+        updatableProduct.setPrice(productValidator.priceChanger(product).getPrice());
+        updatableProduct.setAvailability(product.isAvailability());
+        return updatableProduct;
+    }
+}
---
+++ src/test/java/com/lognex/productrest/controller/ProductControllerIntegrationTest.java
@@ -0,0 +1,257 @@
+package com.lognex.productrest.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.lognex.productrest.dao.ProductRepository;
+import com.lognex.productrest.entity.Product;
+import com.lognex.productrest.exception.CustomEntityNotFoundException;
+import com.lognex.productrest.service.ProductRestServiceImpl;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Description;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Arrays;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class ProductControllerIntegrationTest {
+
+    protected final static UUID TEST_PRODUCT_ID = UUID.fromString("a935b53c-61b9-45d0-be81-27b10f1dca6e");
+    private final static UUID TEST_NON_EXISTENT_PRODUCT_ID = UUID.fromString("1040f406-97b8-490b-bd25-7168abcacfa0");
+    protected final static UUID TEST_NEW_PRODUCT_ID = UUID.fromString("31dedd55-6175-46ed-ad09-cbcbce65d7a8");
+    protected final static String TEST_CORRECT_NAME = "test product";
+    protected final static String TEST_NEW_CORRECT_NAME = "updated test product";
+    protected final static String TEST_CORRECT_DESCRIPTION = "test description";
+    protected final static String TEST_NEW_CORRECT_DESCRIPTION = "updated test description";
+    protected final static BigDecimal TEST_CORRECT_PRICE = BigDecimal.valueOf(1234.56);
+    protected final static BigDecimal TEST_NEW_CORRECT_PRICE = BigDecimal.valueOf(6543.21);
+    private final static boolean TEST_AVAILABILITY_TRUE = true;
+    private final static boolean TEST_AVAILABILITY_FALSE = false;
+    private final static BigDecimal TEST_NULL_PRICE = null;
+    private static final BigDecimal TEST_INCORRECT_PRICE_WITH_MORE_THAN_2_DECIMAL_PLACES = BigDecimal.valueOf(666.666666);
+    @Autowired
+    private ProductRepository repository;
+
+    @Autowired
+    private ProductRestServiceImpl productRestServiceImpl;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    // Возможно это стоит удалить, иначе каждый прогон тестов удалит данные из локальной БД
+    @AfterEach
+    public void resetDb() {
+        repository.deleteAll();
+    }
+
+    @Test
+    @Description("Проверяет, что при передаче корректных данных для создания продукта" +
+            "возвращается созданный корректный продукт")
+    public void test_givenProduct_whenAdd_thenStatus200andProductReturned() throws Exception {
+
+        Product product = new Product(TEST_PRODUCT_ID, TEST_CORRECT_NAME, TEST_CORRECT_DESCRIPTION, TEST_CORRECT_PRICE, TEST_AVAILABILITY_TRUE);
+
+        mockMvc.perform(
+                post("/api/products/product")
+                        .content(objectMapper.writeValueAsString(product)).contentType(MediaType.APPLICATION_JSON)
+        )
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(product.getId().toString()))
+                .andExpect(jsonPath("$.name").value(product.getName()))
+                .andExpect(jsonPath("$.description").value(product.getDescription()))
+                .andExpect(jsonPath("$.price").value(product.getPrice()))
+                .andExpect(jsonPath("$.availability").value(product.isAvailability()));
+        assertEquals(TEST_PRODUCT_ID, productRestServiceImpl.readProduct(TEST_PRODUCT_ID).getId());
+        assertEquals(TEST_CORRECT_NAME, productRestServiceImpl.readProduct(TEST_PRODUCT_ID).getName());
+        assertEquals(TEST_CORRECT_DESCRIPTION, productRestServiceImpl.readProduct(TEST_PRODUCT_ID).getDescription());
+        assertEquals(TEST_CORRECT_PRICE, productRestServiceImpl.readProduct(TEST_PRODUCT_ID).getPrice());
+        assertEquals(TEST_AVAILABILITY_TRUE, productRestServiceImpl.readProduct(TEST_PRODUCT_ID).isAvailability());
+    }
+
+    @Test
+    @Description("Проверяет, что при передаче цены с более чем 2-мя знаками после запятой при создании продукта " +
+            "происходит округление и возвращается продукт с ценой с 2-мя знаками после запятой")
+    public void test_givenIncorrectPriceType_whenAdd_thenStatus200AndPriceCorrect() throws Exception {
+        Product product = new Product(TEST_PRODUCT_ID,
+                TEST_CORRECT_NAME,
+                TEST_CORRECT_DESCRIPTION,
+                TEST_INCORRECT_PRICE_WITH_MORE_THAN_2_DECIMAL_PLACES,
+                TEST_AVAILABILITY_FALSE);
+
+        mockMvc.perform(post("/api/products/product")
+                        .content(objectMapper.writeValueAsString(product))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.price").value(666.67));
+        assertEquals(BigDecimal.valueOf(666.67), productRestServiceImpl.readProduct(TEST_PRODUCT_ID).getPrice());
+    }
+
+    @Test
+    @Description("Проверяет, что при передаче цены равной NULL при создании продукта создается продукт с ценой 0")
+    public void test_givenNullPrice_whenAdd_thenStatus200AndPriceBecomesZero() throws Exception {
+        Product product = new Product(TEST_PRODUCT_ID,
+                TEST_CORRECT_NAME,
+                TEST_CORRECT_DESCRIPTION,
+                TEST_NULL_PRICE,
+                TEST_AVAILABILITY_FALSE);
+
+        mockMvc.perform(post("/api/products/product")
+                        .content(objectMapper.writeValueAsString(product))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.price").value(0.00));
+        assertEquals(BigDecimal.ZERO.setScale(2, RoundingMode.HALF_UP), productRestServiceImpl.readProduct(TEST_PRODUCT_ID).getPrice());
+    }
+
+    @Test
+    @Description("Проверяет, что при получении пустого продукта во время создания падает ошибка")
+    public void test_givenEmptyProduct_whenAdd_thenException() throws Exception {
+        Product product = new Product();
+
+        mockMvc.perform(post("/api/products/product")
+                .content(objectMapper.writeValueAsString(product))
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value("Method arguments not valid"));
+    }
+
+    @Test
+    @Description("Проверяем, что при попытке получить по корректному id существующий продукт, в ответе придет " +
+            "корректное тело этого продукта")
+    public void test_givenId_whenGetExistingProduct_thenStatus200andProductReturned() throws Exception {
+        UUID id = createTestProduct(TEST_PRODUCT_ID, TEST_CORRECT_NAME, TEST_CORRECT_DESCRIPTION, TEST_CORRECT_PRICE, TEST_AVAILABILITY_TRUE).getId();
+
+        mockMvc.perform(
+                get("/api/products/product/{id}", id))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(String.valueOf(TEST_PRODUCT_ID)))
+                .andExpect(jsonPath("$.name").value(TEST_CORRECT_NAME))
+                .andExpect(jsonPath("$.description").value(TEST_CORRECT_DESCRIPTION))
+                .andExpect(jsonPath("$.price").value(TEST_CORRECT_PRICE))
+                .andExpect(jsonPath("$.availability").value(TEST_AVAILABILITY_TRUE));
+    }
+
+    @Test
+    @Description("Проверяем, что если продукта не существует, при попытке его получить будет выброшена ошибка 404")
+    public void test_givenId_whenGetNotExistingProduct_thenStatus404anExceptionThrown() throws Exception {
+
+        mockMvc.perform(
+                get("/api/products/product/{invalid_id}", TEST_NON_EXISTENT_PRODUCT_ID))
+                .andExpect(status().isNotFound())
+                .andExpect(mvcResult -> mvcResult.getResolvedException().getClass().equals(CustomEntityNotFoundException.class));
+    }
+    @Test
+    @Description("Проверяем, что при попытке изменить существующий продукт, возвращается новое тело измененного продукта")
+    public void test_giveProduct_whenUpdate_thenStatus200andUpdatedReturns() throws Exception {
+        UUID id = createTestProduct(TEST_PRODUCT_ID, TEST_CORRECT_NAME, TEST_CORRECT_DESCRIPTION, TEST_CORRECT_PRICE, TEST_AVAILABILITY_TRUE).getId();
+
+        mockMvc.perform(
+                put("/api/products/product/{id}", id)
+                        .content(objectMapper.writeValueAsString(new Product(TEST_PRODUCT_ID, TEST_NEW_CORRECT_NAME,
+                                TEST_NEW_CORRECT_DESCRIPTION, TEST_NEW_CORRECT_PRICE, TEST_AVAILABILITY_FALSE)))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(TEST_PRODUCT_ID.toString()))
+                .andExpect(jsonPath("$.name").value(TEST_NEW_CORRECT_NAME))
+                .andExpect(jsonPath("$.description").value(TEST_NEW_CORRECT_DESCRIPTION))
+                .andExpect(jsonPath("$.price").value(TEST_NEW_CORRECT_PRICE))
+                .andExpect(jsonPath("$.availability").value(TEST_AVAILABILITY_FALSE));
+        assertEquals(TEST_PRODUCT_ID, productRestServiceImpl.readProduct(TEST_PRODUCT_ID).getId());
+        assertEquals(TEST_NEW_CORRECT_NAME, productRestServiceImpl.readProduct(TEST_PRODUCT_ID).getName());
+        assertEquals(TEST_NEW_CORRECT_DESCRIPTION, productRestServiceImpl.readProduct(TEST_PRODUCT_ID).getDescription());
+        assertEquals(TEST_NEW_CORRECT_PRICE, productRestServiceImpl.readProduct(TEST_PRODUCT_ID).getPrice());
+        assertEquals(TEST_AVAILABILITY_FALSE, productRestServiceImpl.readProduct(TEST_PRODUCT_ID).isAvailability());
+    }
+
+    @Test
+    @Description("Проверяем, что при попытке передать для изменения цену с более чем 2 знаками после запятой" +
+            ", цена будет округлена, знаков останется 2 и вернется новое тело продукта")
+    public void test_whenGivenPriceWithMoreThanTwoDecimals_andUpdate_thenPriceHasTwoDecimalsAndUpdateProduct_OK() throws Exception {
+        UUID id = createTestProduct(TEST_PRODUCT_ID, TEST_CORRECT_NAME, TEST_CORRECT_DESCRIPTION, TEST_CORRECT_PRICE, TEST_AVAILABILITY_TRUE).getId();
+
+        mockMvc.perform(
+                        put("/api/products/product/{id}", id)
+                                .content(objectMapper.writeValueAsString(new Product(TEST_PRODUCT_ID, TEST_NEW_CORRECT_NAME,
+                                        TEST_NEW_CORRECT_DESCRIPTION, TEST_INCORRECT_PRICE_WITH_MORE_THAN_2_DECIMAL_PLACES, TEST_AVAILABILITY_FALSE)))
+                                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(TEST_PRODUCT_ID.toString()))
+                .andExpect(jsonPath("$.name").value(TEST_NEW_CORRECT_NAME))
+                .andExpect(jsonPath("$.description").value(TEST_NEW_CORRECT_DESCRIPTION))
+                .andExpect(jsonPath("$.price").value(BigDecimal.valueOf(666.67)))
+                .andExpect(jsonPath("$.availability").value(TEST_AVAILABILITY_FALSE));
+        assertEquals(TEST_PRODUCT_ID, productRestServiceImpl.readProduct(TEST_PRODUCT_ID).getId());
+        assertEquals(TEST_NEW_CORRECT_NAME, productRestServiceImpl.readProduct(TEST_PRODUCT_ID).getName());
+        assertEquals(TEST_NEW_CORRECT_DESCRIPTION, productRestServiceImpl.readProduct(TEST_PRODUCT_ID).getDescription());
+        assertEquals(BigDecimal.valueOf(666.67), productRestServiceImpl.readProduct(TEST_PRODUCT_ID).getPrice());
+        assertEquals(TEST_AVAILABILITY_FALSE, productRestServiceImpl.readProduct(TEST_PRODUCT_ID).isAvailability());
+    }
+
+    @Test
+    @Description("Проверяем, что при попытке изменить цену существующего продукта на NULL, цена изменится на 0 и " +
+            "вернется корректное тело продукта")
+    public void test_giveProductWithNullPrice_whenUpdate_thenPriceBecomesZeroStatus200AndUpdatedReturns() throws Exception {
+        UUID id = createTestProduct(TEST_PRODUCT_ID, TEST_CORRECT_NAME, TEST_CORRECT_DESCRIPTION, TEST_CORRECT_PRICE, TEST_AVAILABILITY_TRUE).getId();
+
+        mockMvc.perform(
+                        put("/api/products/product/{id}", id)
+                                .content(objectMapper.writeValueAsString(new Product(TEST_PRODUCT_ID, TEST_NEW_CORRECT_NAME,
+                                        TEST_NEW_CORRECT_DESCRIPTION, TEST_NULL_PRICE, TEST_AVAILABILITY_FALSE)))
+                                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(TEST_PRODUCT_ID.toString()))
+                .andExpect(jsonPath("$.name").value(TEST_NEW_CORRECT_NAME))
+                .andExpect(jsonPath("$.description").value(TEST_NEW_CORRECT_DESCRIPTION))
+                .andExpect(jsonPath("$.price").value(0.00))
+                .andExpect(jsonPath("$.availability").value(TEST_AVAILABILITY_FALSE));
+        assertEquals(TEST_PRODUCT_ID, productRestServiceImpl.readProduct(TEST_PRODUCT_ID).getId());
+        assertEquals(TEST_NEW_CORRECT_NAME, productRestServiceImpl.readProduct(TEST_PRODUCT_ID).getName());
+        assertEquals(TEST_NEW_CORRECT_DESCRIPTION, productRestServiceImpl.readProduct(TEST_PRODUCT_ID).getDescription());
+        assertEquals(BigDecimal.ZERO.setScale(2, RoundingMode.HALF_UP), productRestServiceImpl.readProduct(TEST_PRODUCT_ID).getPrice());
+        assertEquals(TEST_AVAILABILITY_FALSE, productRestServiceImpl.readProduct(TEST_PRODUCT_ID).isAvailability());
+    }
+
+    @Test
+    @Description("Проверям, что при удалении существующего продукта возвращается ответ 200, а при запросе уже удаленного" +
+            "продукта возвращается 404 ошибка")
+    public void test_givenProduct_whenDeleteProduct_thenStatus200() throws Exception {
+        UUID id = createTestProduct(TEST_PRODUCT_ID, TEST_CORRECT_NAME, TEST_CORRECT_DESCRIPTION, TEST_CORRECT_PRICE, TEST_AVAILABILITY_TRUE).getId();
+
+        mockMvc.perform(
+                delete("/api/products/product/{id}", id))
+                .andExpect(status().isOk());
+        mockMvc.perform(get("/api/products/product/{id}", id))
+                .andExpect(mvcResult -> mvcResult.getResolvedException().getClass().equals(CustomEntityNotFoundException.class));
+    }
+
+    @Test
+    @Description("Проверяем, что при запросе всего списка продуктов этот список возвращается")
+    public void test_givenProducts_whenGetProducts_thenStatus200() throws Exception {
+        Product firstProduct = createTestProduct(TEST_PRODUCT_ID, TEST_CORRECT_NAME, TEST_CORRECT_DESCRIPTION, TEST_CORRECT_PRICE, TEST_AVAILABILITY_TRUE);
+        Product secondProduct = createTestProduct(TEST_NEW_PRODUCT_ID, TEST_NEW_CORRECT_NAME, TEST_NEW_CORRECT_DESCRIPTION, TEST_NEW_CORRECT_PRICE, TEST_AVAILABILITY_FALSE);
+
+        mockMvc.perform(
+                get("/api/products/product"))
+                .andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(Arrays.asList(firstProduct, secondProduct))));
+    }
+
+    private Product createTestProduct(UUID id, String name, String description, BigDecimal price, boolean availability) {
+        Product testProduct = new Product(id, name, description, price, availability);
+        return repository.save(testProduct);
+    }
+}
